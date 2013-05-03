84b848d445d140fb6d7449ccc2ce3054846fc303
Takayuki Sei <takayuki_sei@voyagegroup.com>
2013-05-03 20:58:12 +0900

    added 画像をもったrowクラス

---
+++ src/com/daybysay/helloproject/RowInImage.java
@@ -0,0 +1,17 @@
+package com.daybysay.helloproject;
+
+import android.graphics.Bitmap;
+
+public class RowInImage extends Row {
+	private Bitmap img;
+
+	public Bitmap getImg() {
+		return img;
+	}
+
+	public void setImg(Bitmap img) {
+		this.img = img;
+	}
+	
+
+}
